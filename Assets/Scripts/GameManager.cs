edde89a1503fdb10341800710b84dfaaf9025799
Vanne Vega <manelp3214@gmail.com>
2022-09-29 20:44:34 -0500

    terminar

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState {
+    menu,
+    inGame,
+    gameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState = GameState.menu;
+
+    public static GameManager sharedInstance;
+
+    PlayerController controller;
+
+    void Awake() 
+    {
+        if(sharedInstance == null) {
+             sharedInstance = this;
+         }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      controller = GameObject.Find("Player").
+      GetComponent<PlayerController>();  
+    }
+
+    // Update is called once per frame
+    void Update(){
+      if(Input.GetButtonDown("Submit") && 
+      currentGameState != GameState.inGame) {
+        StartGame();
+      }  
+    }
+    public void StartGame() {
+         SetGameState(GameState.inGame);
+
+    }
+    public void GameOver() {
+         SetGameState(GameState.gameOver);
+
+    }
+    public void BackToMenu() {
+         SetGameState(GameState.menu);
+
+    }
+    
+    void SetGameState(GameState newGameState) {
+        if(newGameState == GameState.menu) {
+            //TODO: colocar la logica del menu
+        }else if(newGameState == GameState.inGame) {
+            //TODO: hay que preparar la escena para jugar
+           controller.StartGame();
+        }else if(newGameState == GameState.gameOver) {
+            //TODO: preparar el juego para el Game Over
+        }
+        this.currentGameState = newGameState;
+    }
+}
